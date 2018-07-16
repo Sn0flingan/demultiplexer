c8cb7ce7c3bc8c2450b35d447cb1fff052d81cb7
Sn0flingan <alice.anlind@gmail.com>
2018-07-16 15:07:52 +0200

    Add primer matching and result to console

--- demultiplexer.py
+++ demultiplexer.py
@@ -6,16 +6,22 @@
 #
 
 import argparse
+from Levenshtein import distance
 
 def main():
     args = get_arguments()
+    primer_f = "TTGATTACGTCCCTGCCCTTT"
+    primer_r = "CCTTAGTAACGGCGAGTGAAA" #reverse compliment of reverse primer
     with open(args.input) as file:
         read_next_line = False
         for line in file:
             if line[0]=='@':
                 read_next_line = True
             elif read_next_line:
-                check_barcode(line)
+                read_start = line[:100]
+                check_barcode(read_start, primer_f)
+                read_end = line[-100:-1] #-1 due to newline character as last character of read
+                check_barcode(read_end, primer_r)
                 read_next_line = False
 
 
@@ -31,10 +37,16 @@ def get_arguments():
         print("Verbosity: {}".format(args.verbosity))
     return args
 
-def check_barcode(read):
-    start_seq = read[:100]
-    end_seq = read[-100:-1] #-1 due to newline character as last character of read
-    print(start_seq)
-    print(end_seq)
+def check_barcode(sequence, primer):
+    min_dist = len(sequence)
+    best_match_idx = 0
+    for i in range(0,len(sequence)-len(primer)):
+        seq_dist = distance(sequence[i:i+len(primer)],primer)
+        if seq_dist<min_dist:
+            best_match_idx = i
+            min_dist = seq_dist
+    coloured_match = '\x1b[6;31;48m' + sequence[best_match_idx:best_match_idx+len(primer)] + '\x1b[0m'
+    print(sequence[:best_match_idx] + coloured_match + sequence[best_match_idx+len(primer):] )
+    print("Distance : {}".format(min_dist))
 
 main()
