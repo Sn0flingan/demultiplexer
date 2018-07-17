29c33e59a3ab085ae624c4279a7667c2f5f7232f
Sn0flingan <alice.anlind@gmail.com>
2018-07-17 10:06:34 +0200

    Add histogram over position of primer matching

--- demultiplexer.py
+++ demultiplexer.py
@@ -7,12 +7,14 @@
 
 import argparse
 from Levenshtein import distance
+import seaborn as sns
 
 def main():
     args = get_arguments()
     primer_f = "TTGATTACGTCCCTGCCCTTT"
     primer_r = "CCTTAGTAACGGCGAGTGAAA" #reverse compliment of reverse primer
     matches = {'Leading':{'Both':0, 'Start':0, 'End':0}, 'Lagging': {'Both':0, 'Start':0, 'End':0}, 'None': 0}
+    index = {'Matching-start': [], 'Matching-end': [] }
     with open(args.input) as file:
         read_next_line = False
         for line in file:
@@ -25,27 +27,39 @@ def main():
                 print("--- Read ---")
                 print("Sequence len: {}".format(len(line)))
                 print("-- Leading strand")
-                f_dist_s = check_barcode(read_start, primer_f)
-                f_dist_e = check_barcode(read_end, primer_r)
+                (f_dist_s, f_idx_s) = check_barcode(read_start, primer_f)
+                (f_dist_e, f_idx_e) = check_barcode(read_end, primer_r)
                 print("-- Lagging strand")
-                r_dist_s = check_barcode(read_start, rev_comp(primer_r))
-                r_dist_e = check_barcode(read_end, rev_comp(primer_f))
-                if f_dist_s<=6 and f_dist_e<=6:
+                (r_dist_s, r_idx_s) = check_barcode(read_start, rev_comp(primer_r))
+                (r_dist_e, r_idx_e) = check_barcode(read_end, rev_comp(primer_f))
+                if f_dist_s<=7 and f_dist_e<=7:
                     matches['Leading']['Both'] +=1
-                elif r_dist_s<=6 and r_dist_e<=6:
+                    index['Matching-start'].append(f_idx_s)
+                    index['Matching-end'].append(f_idx_e)
+                elif r_dist_s<=7 and r_dist_e<=7:
                     matches['Lagging']['Both'] +=1
-                elif f_dist_s<=6 and f_dist_e>6:
+                    index['Matching-start'].append(r_idx_s)
+                    index['Matching-end'].append(r_idx_e)
+                elif f_dist_s<=7 and f_dist_e>7:
                     matches['Leading']['Start'] +=1
-                elif f_dist_s>6 and f_dist_e<=6:
+                    index['Matching-start'].append(f_idx_s)
+                elif f_dist_s>7 and f_dist_e<=7:
                     matches['Leading']['End'] +=1
-                elif r_dist_s<=6 and r_dist_e>6:
+                    index['Matching-end'].append(f_idx_e)
+                elif r_dist_s<=7 and r_dist_e>7:
                     matches['Lagging']['Start'] +=1
-                elif r_dist_s>6 and r_dist_e<=6:
+                    index['Matching-start'].append(r_idx_s)
+                elif r_dist_s>7 and r_dist_e<=7:
                     matches['Lagging']['End'] +=1
+                    index['Matching-end'].append(r_idx_e)
                 else:
                     matches['None'] +=1
                 read_next_line = False
     print(matches)
+    histogram_plot(index['Matching-start'], "Start")
+    histogram_plot(index['Matching-end'], "End")
+    #print(sorted(index['Matching']))
+
 
 
 def get_arguments():
@@ -71,11 +85,16 @@ def check_barcode(sequence, primer):
     coloured_match = '\x1b[6;31;48m' + sequence[best_match_idx:best_match_idx+len(primer)] + '\x1b[0m'
     #print(sequence[:best_match_idx] + coloured_match + sequence[best_match_idx+len(primer):] )
     print("Distance : {}".format(min_dist))
-    return min_dist
+    return (min_dist, best_match_idx)
 
 def rev_comp(seq):
     complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     rev_comp_seq = "".join(complement.get(base,base) for base in reversed(seq))
     return rev_comp_seq
 
+def histogram_plot(list, name):
+    sns.set(style="whitegrid")
+    plot = sns.distplot(list, kde=False, color="b")
+    fig = plot.get_figure()
+    fig.savefig(name + "_histogram.png")
 main()
